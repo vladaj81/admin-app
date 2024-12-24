b64cfee05e3a1990ade9741eb33300ea4a585783
vladaj13 <vladaj13@gmail.com>
2024-12-24 14:57:49 +0100

    Creating admin user seeder

---
+++ database/seeders/CreateAdminUserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+  
+namespace Database\Seeders;
+  
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+  
+class CreateAdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::create([
+            'name' => 'Admin User', 
+            'email' => 'admin@test.com',
+            'password' => bcrypt('admin12345')
+        ]);
+        
+        $role = Role::create(['name' => 'Admin']);
+         
+        $permissions = Permission::pluck('id','id')->all();
+       
+        $role->syncPermissions($permissions);
+         
+        $user->assignRole([$role->id]);
+    }
+}
