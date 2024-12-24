8cb89c016905ce915c66e05c0a76aa576cf8d611
vladaj13 <vladaj13@gmail.com>
2024-12-24 12:47:32 +0100

    Adding application routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\DataImportController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +12,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => ['auth']], function() {
+    Route::resource('permissions', PermissionController::class);
+    Route::resource('users', UserController::class);
+    Route::get('/data-import', [DataImportController::class, 'index'])->name('data-import');
+    Route::post('/data-import', [DataImportController::class, 'importFiles'])->name('import-files');
+    Route::get('/get-required-headers', [DataImportController::class, 'getImportTypeRequiredHeaders'])->name('get-required-headers');
+});
